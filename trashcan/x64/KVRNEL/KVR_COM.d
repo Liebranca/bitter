0be378608c159711e72524d0fbad3c1439dbed23
Liebranca <38820379+Liebranca@users.noreply.github.com>
2021-03-03 09:44:38 -0300

    Delete trashcan/x64 directory

--- trashcan/x64/KVRNEL/KVR_COM.d
+++
@@ -1,4 +0,0 @@
-D:\lieb_git\KVR\trashcan\x64\KVRNEL\KVR_COM.o: \
- D:\lieb_git\KVR\src\KVRNEL\MEM\KVR_COM.c \
- D:\lieb_git\KVR\src\KVRNEL\MEM\KVR_COM.h \
- D:\lieb_git\KVR\src\KVRNEL\MEM\KVR_EVIL.h
--- trashcan/x64/__main__/TEST.d
+++
@@ -1,3 +0,0 @@
-D:\lieb_git\KVR\trashcan\x64\__main__\TEST.o: D:/lieb_git/KVR/src/TEST.c \
- D:/lieb_git/KVR/src/KVRNEL/MEM/KVR_BIN.h \
- D:/lieb_git/KVR/src/KVRNEL/MEM/KVR_EVIL.h
