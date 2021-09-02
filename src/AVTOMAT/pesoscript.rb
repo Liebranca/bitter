dfe13d310496206445237cd8eade6624f85a49e4
Liebranca <dantepaola25@gmail.com>
2021-09-02 09:32:30 -0300

    Parsing & regexes
    Not saving the actual parsing data (so what-is-what in an expression), but doing so is trivial.

--- src/AVTOMAT/pesoscript.rb
+++ src/AVTOMAT/pesoscript.rb
@@ -30,11 +30,11 @@ LEX        = {};
 #   ---     ---     ---     ---     ---
 
 class READER
-    attr_accessor :act, :tokens, :keys
+    attr_accessor :act, :tokens, :keys, :rules, :forms
     def initialize
 
         @lvl    = 0;
-        @ord    = 0;
+        @elm    = 0;
         @state  = 0;
         @skip   = 0;
 
@@ -49,6 +49,10 @@ class READER
         @tokens = [];
 
         @keys   = {};
+        @rules  = {};
+        @forms  = {};
+        @formi  = 0;
+
         @onins  = nil;
 
         @peopcd = nil;
@@ -61,9 +65,63 @@ class READER
 
 #   ---     ---     ---     ---     ---
 
-    def stkey(val)
-        @keys[val] = [];
-        @onins     = ->(t) { @keys["#{val}"].append(t); }
+    def parse(s)
+
+        i=0; @forms.each_key { |k|
+            @forms[k].each { |f|
+                m=s.match(eval("/#{f}/"));
+                if(m.to_s==s);
+                    puts "#{s} is a form ##{i} #{k};\nMatching reg /#{f}/";
+                    return 1;
+
+                end; i+=1;
+
+            };
+        }; return 0;
+
+    end;
+
+#   ---     ---     ---     ---     ---
+
+    def stcol(wr, val)
+        eval("@#{wr}['#{val}'] = ''");
+        @onins = ->(t) {
+            sep="|"; if(@cchar==';'); sep=''; end;
+            eval("@#{wr}['#{val}'] << '\\b#{t}\\b#{sep}';");
+
+        }
+
+    end;
+
+    def stform(val)
+        @forms[val] = [""]
+        @formi      = 0;
+
+        @onins      = ->(t) {
+
+            if(t[0]=='<' && t[-1]=='>')     # is tag
+                tag=t[1..-2];
+
+#   ---     ---     ---     ---     ---
+
+                if   (@keys.include?(tag))
+                    forms["#{val}"][@formi] << "(#{@keys[tag]}) "
+
+                elsif(@rules.include?(tag))
+                    forms["#{val}"][@formi] << "#{@rules[tag][0..-2]} "
+
+                else
+                    puts "Invalid tag #{t} in form <#{val}>"
+
+                end;
+
+            elsif(t==';')
+                forms["#{val}"][@formi]=@forms["#{val}"][@formi][0..-2];
+                @formi+=1; forms["#{val}"].append("");
+
+            end;
+
+        }
 
     end;
 
@@ -110,22 +168,27 @@ class READER
             self.tkins;
             @state&=~PE_LITERAL;
             return;
+
         end;
 
         if(@skip==1)
-            @skip = 0;
+            @skip=0;
             return;
 
         end;
 
-        if(@cchar!='\\'); @ctokn << @cchar; end;
+        if( ( @cchar!='\\'               ) \
+        ||  (@cchar=='\\' && @lchar=='\\') )
+            @ctokn << @cchar;
+
+        end;
 
     end;
 
 #   ---     ---     ---     ---     ---
 
     def isliteral
-        if(@cchar=='\\')
+        if(@cchar=='\\' && (@state&PE_LITERAL)==0)
             @state|=PE_LITERAL;
             return 1;
 
@@ -243,7 +306,7 @@ class READER
 
                 @skip=1;
 
-            else
+            elsif((@state&PE_LITERAL)==0)
                 self.tkins;
                 @onins=nil;
                 @ctokn="#EE"; self.tkins;
@@ -290,14 +353,14 @@ class READER
 #   ---     ---     ---     ---     ---
 
     def RQ_OPEN(c)
-        s="#{c} #{@ord}"
-        @ord+=1; return s;
+        s="#{c} #{@elm}"
+        @elm+=1; return c;
 
     end;
 
     def RQ_CLSE(c)
-        @ord-=1;
-        return "#{c} #{@ord}"
+        @elm-=1;
+        return c; #"#{c} #{@elm}"
 
     end;
 
@@ -305,13 +368,13 @@ class READER
 
     def SQ_OPEN(c)
         s="#{c} #{@lvl}"
-        @lvl+=1; return s;
+        @lvl+=1; return c;
 
     end;
 
     def SQ_CLSE(c)
         @lvl-=1;
-        return "#{c} #{@lvl}"
+        return c; #"#{c} #{@lvl}"
 
     end;
 
@@ -330,14 +393,16 @@ end;
 
 #   ---     ---     ---     ---     ---
 
-LEX["("  ]=PEOP.new(0x00000000, ->(x) { perd.RQ_OPEN(x) } );
-LEX[")"  ]=PEOP.new(0x00000000, ->(x) { perd.RQ_CLSE(x) } );
-LEX["{"  ]=PEOP.new(0x00000000, ->(x) { perd.SQ_OPEN(x) } );
-LEX["}"  ]=PEOP.new(0x00000000, ->(x) { perd.SQ_CLSE(x) } );
+LEX["("   ]=PEOP.new(0x00000000, ->(x) { perd.RQ_OPEN(x)          } );
+LEX[")"   ]=PEOP.new(0x00000000, ->(x) { perd.RQ_CLSE(x)          } );
+LEX["{"   ]=PEOP.new(0x00000000, ->(x) { perd.SQ_OPEN(x)          } );
+LEX["}"   ]=PEOP.new(0x00000000, ->(x) { perd.SQ_CLSE(x)          } );
 
-LEX["itr"]=PEOP.new(0x00000002, nil                       );
-LEX["lex"]=PEOP.new(0x00000000, ->(x) { perd.lexbit (x) } );
-LEX["key"]=PEOP.new(0x00000001, ->(x) { perd.stkey  (x) } );
+LEX["itr" ]=PEOP.new(0x00000002, nil                                );
+LEX["lex" ]=PEOP.new(0x00000000, ->(x) { perd.lexbit(x)           } );
+LEX["key" ]=PEOP.new(0x00000001, ->(x) { perd.stcol("keys",  x)   } );
+LEX["rule"]=PEOP.new(0x00000001, ->(x) { perd.stcol("rules", x)   } );
+LEX["form"]=PEOP.new(0x00000001, ->(x) { perd.stform(x)  } );
 
 LEX["chk_in"] = ->(var_name) {
     Module.constants.include?(var_name.to_sym);
@@ -369,7 +434,5 @@ File.foreach(fpath) { |pe|
 
     }; if(abort==1); break; end;
 };
-
-perd.keys.each_key { |k| puts "#{k}\n"; print perd.keys[k]; puts "\n\n"; }
 
 #   ---     ---     ---     ---     ---
