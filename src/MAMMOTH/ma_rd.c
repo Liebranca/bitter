0f15868ccc6232a872b8a3771bb7b9d042a75cc8
Liebranca <dantepaola25@gmail.com>
2021-09-30 06:35:18 -0300

    Data-type decomposition

--- src/MAMMOTH/ma_rd.c
+++ src/MAMMOTH/ma_rd.c
@@ -25,6 +25,7 @@
 #include <string.h>
 
 //   ---     ---     ---     ---     ---
+// state flags...
 
 #define MAMMIT_SF_PESO 0x000000FF
 
@@ -39,6 +40,129 @@
 
 #define MAMMIT_SF_CCLA 0x00000800
 #define MAMMIT_SF_CFUN 0x00001000
+
+// (...)
+
+// constants
+#ifndef MAMMIT_TK_WIDTH
+    #define MAMMIT_TK_WIDTH 64
+
+#endif
+
+#ifndef MAMMIT_TK_COUNT
+    #define MAMMIT_TK_COUNT 16
+
+#endif
+
+//   ---     ---     ---     ---     ---
+
+static uchar  tokens[MAMMIT_TK_COUNT][MAMMIT_TK_WIDTH];
+
+static uchar* rd_tk   = tokens[0];          // components of current statement
+static uint   rd_tki  = 0;                  // num of tokens
+static uint   rd_tkp  = 0;                  // token char idex
+static uint   rd_tkx  = 0;                  // (iter) current token idex
+
+static uchar* rd_buff = NULL;               // char buffer containing statements
+
+static uchar  rd_cur  = '\0';               // current char in buff
+static uchar  rd_prv  = '\0';               // previous char in buff
+static uchar  rd_nxt  = '\0';               // next char in buff
+
+static ushort rd_wid  = 0x0000;             // next | (cur<<8)
+static uint   rd_pos  = 0;                  // position into buffer
+
+//   ---     ---     ---     ---     ---
+
+typedef struct MAMM_TYPEVAL_DATA {          // to simplify reading wacky types
+
+    uchar base[MAMMIT_TK_WIDTH];            // type-name
+
+    union {
+        struct {
+            uchar indlvl;                   // indirection level (ptr depth!)
+            uchar arrsize;                  // >1 equals pow(2, arrsize), else 1
+            uchar flags;                    // static, const, unsigned...
+            uchar pad;                      // idk what to do with the last byte
+
+        };  uint  F;
+    };
+
+} TYPEDATA; static TYPEDATA typedata = {0};
+
+//   ---     ---     ---     ---     ---
+
+void UPKTYPE(uchar* typeval)                {
+
+    CLMEM2(                                 // clean-up leftovers from previous use
+        (void*) &typedata,
+        sizeof(TYPEDATA)
+
+    );
+
+    uchar* base = typedata.base+0;          // point to our static block of chars
+    uint   len  = strlen(typeval);          // length of original string
+
+    ushort i    = 0;                        // offset into original string
+    ushort j    = 0;                        // offset into __target__ string
+
+    uchar  c    = 0x00;                     // character currently being pointed at
+
+//   ---     ---     ---     ---     ---
+
+    TOP:                                    // read through original string and decompose
+
+        c=typeval[i];                       // get next char
+
+        if((c >= 0x30) && (0x39 >= c)) {    // if char is number
+            typedata.arrsize*=10;           // left-shift
+            typedata.arrsize+=c-0x30;       // add unit
+            goto BOT;                       // skip to end
+
+        }; if(j || i>2) { goto MID; };      // skip if char > original[0-2]
+
+//   ---     ---     ---     ---     ---
+
+    switch(c) {
+
+        case 0x7A:                          // z, zzzzstatic (lots o' volts)
+            typedata.flags|=0x01; goto BOT;
+        case 0x71:                          // q, qqqqconstant
+            typedata.flags|=0x02; goto BOT;
+        case 0x75:                          // u, uuuuunsigned
+            typedata.flags|=0x04; goto BOT;
+
+        default:
+            break;
+
+    };
+
+//   ---     ---     ---     ---     ---
+
+MID:switch(c) {
+
+        case 0x2A:                          // ptr to ptr to ptr to ptr to ptpr to what
+            typedata.indlvl++; break;
+
+        default:                            // everything else goes
+            base[j]=c; j++; break;
+
+    }; BOT: i++; if(i<len) { goto TOP; }
+
+CALOUT(E,"\
+Base: %s\n\
+Size: %u\n\
+Astr: %u\n\
+Usig: %u\n",
+
+typedata.base,
+typedata.arrsize,
+typedata.indlvl,
+typedata.flags&0x04
+
+);
+
+                                                    };
 
 //   ---     ---     ---     ---     ---
 
@@ -77,6 +201,8 @@ typedef struct MAMM_INTERPRETER {           // smach for pe-text input
 
     };
 
+    uchar  lvla_stack[256];                 // markers for recalling previous context
+
     SYMBOL slots[GNAMES_SZ];                // array of built-ins
     STK    slstack;                         // stack of (free)indices into built-ins array
 
@@ -84,12 +210,40 @@ typedef struct MAMM_INTERPRETER {           // smach for pe-text input
 
 //   ---     ---     ---     ---     ---
 
+#define MAMMIT_LVLA_NXT { mammi->lvla_stack[mammi->lvla]=mammi->cntx; mammi->lvla++; }
+#define MAMMIT_LVLA_PRV { mammi->lvla++; mammi->cntx=mammi->lvla_stack[mammi->lvla]; }
+
+//   ---     ---     ---     ---     ---
+
 void REGMA(void)                            {
-    if(!(mammi->state&MAMMIT_SF_CREG)) {    // set state if unset, then ret
-        mammi->state |= MAMMIT_SF_CREG;
+    if(!(mammi->state&MAMMIT_SF_CREG)) {    // if unset, do and ret
+        mammi->state |= MAMMIT_SF_CREG;     // set state
+
+        rd_tkx++; uchar* name = tokens[rd_tkx];
+        CALOUT(K, "reg %s\n", name);
+
         return;
 
-    }; mammi->state &=~MAMMIT_SF_CREG;      /* effectively else, unset the state */         };
+    }; mammi->state &=~MAMMIT_SF_CREG;      /* effectively, an implicit else */             };
+
+void REGTP(void)                            {
+
+    uchar* type = typedata.base;  rd_tkx++; // fetch, move to next token
+    uchar* name = tokens[rd_tkx]; rd_tkx++; // fetch, move moar
+
+    if(rd_tkx<rd_tki) {                     // if token count > 2 we have an assignment
+        if(tokens[rd_tkx][0] != 0x3D) {     // single equal only
+
+                                            // obligatory pop culture reference
+            CALOUT                          (E, "You are NOT a walrus, John!\n");
+            return;
+
+        }; rd_tkx++;
+
+        // name = expression... goes here
+        return;
+
+    };                                                                                };
 
 //   ---     ---     ---     ---     ---
 
@@ -106,15 +260,23 @@ void NTNAMES(void)                          {
         STACKPUSH(byref(mammi->slstack), x);
 
                                             // nit the hash
-    }; GNAMES_HASH = MKHASH                 (10, "gnames_hash"                         );
+    }; GNAMES_HASH = MKHASH                 (7, "gnames_hash"                          );
 
 //   ---     ---     ---     ---     ---
 
     char* base_dtypes[]={                   // basic types
-        "char",  "uchar",
-        "short", "ushort",
-        "int",   "uint",
-        "long",  "ulong",
+
+        "void" ,
+        "stark",
+        "nihil",
+
+        "char" ,
+        "short",
+        "int"  ,
+        "long" ,
+
+        "float",
+        "double"
 
     };
 
@@ -122,8 +284,7 @@ void NTNAMES(void)                          {
 
                                             // insert basic types into table
         STACKPOP                            (byref(mammi->slstack), y                  );
-
-        mammi->slots[y] = SYMNEW            ("TYPE", base_dtypes[x], NULL              );
+        mammi->slots[y] = SYMNEW            ("TYPE", base_dtypes[x], REGTP             );
         HASHSET                             (GNAMES_HASH, byref(mammi->slots[y].id)    );
 
     };
@@ -157,24 +318,9 @@ void DLNAMES(void)                          { DLMEM(GNAMES_HASH); DLMEM(mammi); 
 
 //   ---     ---     ---     ---     ---
 
-static uchar  tokens[16][64];
-
-static uchar* rd_tk   = tokens[0];          // components of current statement
-static uint   rd_tki  = 0;                  // token idex
-static uint   rd_tkp  = 0;                  // token char idex
-
-static uchar* rd_buff = NULL;               // char buffer containing statements
-
-static uchar  rd_cur  = '\0';               // current char in buff
-static uchar  rd_prv  = '\0';               // previous char in buff
-static uchar  rd_nxt  = '\0';               // next char in buff
-
-static ushort rd_wid  = 0x0000;             // next | (cur<<8)
-static uint   rd_pos  = 0;                  // position into buffer
-
 void CHKTKNS(void)                          {
 
-    uchar* SEQ[16];
+    uchar* SEQ[MAMMIT_TK_COUNT];
 
     uchar* SEQN;
     uint   SEQI;
@@ -183,11 +329,9 @@ void CHKTKNS(void)                          {
 
     if(!(mammi->state&MAMMIT_SF_CNTX)) {    // no current context
 
-        SEQN   = "NON";
-
+        SEQN   = "CNON";
         SEQ[0] = "CNTX\x01";
-        SEQ[1] = "NAME\x01";
-        SEQI   = 2;
+        SEQI   = 1;
 
     }
 
@@ -195,65 +339,96 @@ void CHKTKNS(void)                          {
 
     elif(mammi->state&MAMMIT_SF_CREG) {     // context == reg
 
-        SEQN   = "REG";
-
-        SEQ[0] = "TYPE\x01";
-        SEQ[1] = "NAME\x01";
-        SEQI   = 2;
+        SEQN   = "CREG";
+        SEQ[0] = "TYPE\x02";
+        SEQI   = 1;
 
     };
 
 //   ---     ---     ---     ---     ---
 
     if(SEQI > (rd_tki)) {
-        CALOUT(E, "Invalid token sequence number for context %s", SEQN);
+        CALOUT(E, "Invalid token sequence number for context %s\n", SEQN);
         return;
 
     };
 
 //   ---     ---     ---     ---     ---
 
-    for(uint x=0; x<rd_tki; x++) {
+    for(rd_tkx=0; rd_tkx<rd_tki; rd_tkx++) {
 
         uchar seq_k[5];                     // symbol-type key used as filter into table
         uchar seq_i;                        // number of inputs taken by symbol
 
         for(uint y=0; y<4; y++) {           // unpack SEQ into k and i 
-            seq_k[y]=SEQ[x][y];
+            seq_k[y]=SEQ[rd_tkx][y];
 
-        }; seq_k[4]='\0'; seq_i=SEQ[x][4];  // put that nullterm there
+        }; seq_k[4]='\0';                   // put that nullterm there
+        seq_i=SEQ[rd_tkx][4];               // fetch argcount
 
 //   ---     ---     ---     ---     ---
 
-        int     valid = 1;
-        SYMBOL* sym   = NULL;
+        int       valid = 1;
+        SYMBOL*   sym   = NULL;
 
         if(strcmp(seq_k, "NAME")) {         // names can be anything, live the danger
 
-            void* nulmy;                    // non-naming tokens can carry funcalls
+            void* nulmy = NULL;             // non-naming tokens can carry funcalls
                                             // they must exist within our gnames table, so
                                             // in-invoking any one of them we must fetch
                                             // strictly by key, since strings is all we have
                                             // wait, wat? embedding commands in text? in C?
                                             // well, who would've thought...
 
-                                            // fetch from table __by_key__ (slowest)
-            STR_HASHGET                     (GNAMES_HASH, tokens[x], nulmy, 0     );
+//   ---     ---     ---     ---     ---
+
+            uchar key[MAMMIT_TK_WIDTH];     // fetch this from table
+
+            uint  len=strlen(
+                    tokens[rd_tkx]
+
+            );                              // just so I don't have to do {cpy} while(*t++)
 
 //   ---     ---     ---     ---     ---
+
+            if(!strcmp(seq_k, "TYPE")) {    // corner case: type flags, arrays, pointers
+
+                UPKTYPE(tokens[rd_tkx]);    // decompose type descriptor
+
+                uint x;                     // like a fashion designer, take measures ;>
+                len = strlen                (typedata.base                             );
+
+                for(x=0; x<len; x++) {      // now copy
+                    key[x]=typedata.base[x];// key == base typename
+
+                }; key[x]=0x00;             // put the nullterm there...
+            }
+
+            else { uint x;
+                for(x=0; x<len; x++) {      // now copy
+                    key[x]=tokens[rd_tkx][x];// key == base typename
+
+                }; key[x]=0x00;
+
+            };
+
+//   ---     ---     ---     ---     ---
+
+                                            // fetch from table __by_key__ (slowest)
+            STR_HASHGET                     (GNAMES_HASH, key, nulmy, 0                );
 
             valid = nulmy!=NULL;            // success on fetch means we might have to
             if(valid) {                     // modify mammit state based on symbols read
                 sym = (SYMBOL*) nulmy;      // no sneaky funcalls on invalid input, please
-            };
 
-        };                                  // now hope you didn't make any silly mistakes
+        }};                                 // now hope you didn't make any silly mistakes
 
 //   ---     ---     ---     ---     ---
 
         if(valid) {                         // debugger's falacy:
                                             // "if it prints something, it does something!"
-            CALOUT                          (K, "%u: %s %s\n", x, seq_k, tokens[x]);
+            CALOUT                          (K, "%u: %s %s\n",                         \
+                                             rd_tkx, seq_k, tokens[rd_tkx]             );
 
             if(sym) { if(sym->onrd) {       // this is why I want if x then y syntax
                 sym->onrd();
@@ -262,11 +437,10 @@ void CHKTKNS(void)                          {
         }
 
         else {                              // very much the same as above
-            CALOUT                          (K, "%u: INVALID %s: '%s'\n",         \
-                                             x, seq_k, tokens[x]                  );
+            CALOUT                          (K, "%u: INVALID %s: '%s'\n",              \
+                                             rd_tkx, seq_k, tokens[rd_tkx]             );
 
-        };
-    };                                                                                      };
+    }};                                                                                     };
 
 //   ---     ---     ---     ---     ---
 
@@ -351,10 +525,10 @@ void RDNXT(void)                            {
 
 //   ---     ---     ---     ---     ---    TERMINATORS
 
-        case 0x7B: mammi->lvla++;
+        case 0x7B: MAMMIT_LVLA_NXT
             goto PROCST;
 
-        case 0x7D: mammi->lvla--;
+        case 0x7D: MAMMIT_LVLA_PRV
             goto PROCST;
 
         case 0x3B: PROCST:
@@ -363,10 +537,12 @@ void RDNXT(void)                            {
                 rd_tkp = 0; rd_tki++;       // reset position && advance slot
                 rd_tk  = tokens[rd_tki];    // push token
 
-            }; CHKTKNS();                   // process token sequence and clean up
+            }; if(!rd_tki) { break; }       // no tokens, no checking
 
-            for(uint i=0; i<16; i++) {
-                CLMEM2(tokens[i], 64);
+            CHKTKNS();                      // process token sequence and clean up
+
+            for(uint i=0; i<MAMMIT_TK_COUNT; i++) {
+                CLMEM2(tokens[i], MAMMIT_TK_WIDTH);
 
             }; rd_tkp=0; rd_tki=0;
             rd_tk=tokens[rd_tki]; break;
@@ -385,14 +561,10 @@ int main(void)                              {
     NTNAMES();
     MEM* s=MKSTR("MAMM_RD", 1024, 1); CLMEM(s);
 
-    RPSTR(&s, "reg vars { uint x; }", 0);
+    RPSTR(&s, "reg vars { uint8* x; }", 0);
     rd_buff = MEMBUFF(s, uchar, 0);
 
-    for(uint i=0; i<16; i++) {
-        CLMEM2(tokens[i], 64);
-
-    }; RDNXT();
-
+    RDNXT();
     DLMEM(s);
     DLNAMES();
 
